172ee415a603b95afea8e413e41476097b0dabc3
Luis Arita <github@luisarita.com>
2017-08-04 11:31:19 -0400

    Bug Fix

--- app/assets/javascripts/location.js
+++ app/assets/javascripts/location.js
@@ -21,16 +21,22 @@ function fillInPosition(position) {
 function initializeMap(locations) {
   handler = Gmaps.build('Google');
   handler.buildMap({ provider: {}, internal: {id: 'map'}}, function(){
-    markers = handler.addMarkers(locations);
-    handler.bounds.extendWith(markers);
+    if (locations != null){
+      markers = handler.addMarkers(locations);
+      handler.bounds.extendWith(markers);
+    }
     handler.fitMapToBounds();
-    if (locations.length == 1) {
+    if (locations != null && locations.length == 1) {
       handler.getMap().setZoom(13)
     };
   });
 }
 
 $(document).ready(function() { 
-  var locations = JSON.parse(document.getElementById("locations").value);
-  initializeMap(locations);
+  var locations = null;
+  location_input = document.getElementById("locations")
+  if (location_input != null){
+    locations = JSON.parse(location_input.value);
+  }
+  setTimeout(function(){ initializeMap(locations) }, 1500);  
 });
